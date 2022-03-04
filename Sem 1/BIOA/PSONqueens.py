c6e3ae6d3e90795d818aeafbfb2cc1d43b80c1d7
JerinPaulS <jerinsprograms@gmail.com>
2022-03-04 19:41:57 +0530

    Ensemble completed

--- Sem 1/BIOA/PSONqueens.py
+++ Sem 1/BIOA/PSONqueens.py
@@ -32,10 +32,10 @@ def display(positions):
 		for col in range(N):
 			if (row, col) in positions:
 				temp.append("Q")
-				print(" Q ", end = "")
+				print(" Q ", end="")
 			else:
 				temp.append(" ")
-				print(" . ", end = "")
+				print(" . ", end="")
 		board.append(temp)
 		print()
 
@@ -126,14 +126,16 @@ def PSO():
 	#for i in range(ITERATIONS):
 	i = 0
 	while gbest > 2:
+		if iter_count == 3:
+			break
 		iter_count += 1
 		population = []
 		print("\n\t *** ITERATION " + str(i + 1) + " ***")
-		pop_result = []
+		pop_result = None
 		for p in DATA:
 			temp = []
 			total = 0
-			print(len(p))
+			#print(len(p))
 			for index, individual in enumerate(p):
 				row, col = individual
 				total += fitness(p, row, col)
@@ -142,9 +144,12 @@ def PSO():
 				vy = round(((w * vy) + (c1 * r1 * (pbest - y)) + (c1 * r1 * (gbest - y))), 4)
 				if vx > 8 or vx < 0:
 					vx = random.randint(1, 8)
+				else:
+					vx = int(vx)
 				if vy > 8 or vy < 0:
 					vy = random.randint(1, 8)
-				vx, vy = int(vx), int(vy)
+				else:
+					vy = int(vy)
 				if 0 <= row + vx < N and 0 <= col + vy < N and (row + vx, col + vy) not in p:
 					new_x = row + vx
 					new_y = col + vy
@@ -152,19 +157,42 @@ def PSO():
 			if pbest > total:
 				pbest = total
 				pop_result = p
+				t = 0
+				for v in pop_result:
+					t += fitness(pop_result, v[0], v[1])
+				display(pop_result)
+				print("Local", pop_result, pbest, t)
+
 			temp.append(p)
 			temp.append(total)
 			population.append(temp)
 		if gbest > pbest:
 			gbest = pbest
 			global_result = pop_result
+			t = 0
+			for v in global_result:
+				t += fitness(global_result, v[0], v[1])
+			display(global_result)
+			print("Global", global_result, t)
+		'''
 		display(global_result)
-		print("\t\tGBest " + str(gbest))
+		t = 0
+		for v in global_result:
+			t += fitness(global_result, v[0], v[1])
+		print(global_result, t)
+		'''
+		print("\t\tGBest ", gbest)
 		DATA = select_population(population)
 		i += 1
+
+	print("\n\n\tThis is the result\n")
 	display(global_result)
-	print("\t\tGBest " + str(gbest))
-	print(iter_count)
+	print("\t\tGBest ", gbest)
+	t = 0
+	for v in global_result:
+		t += fitness(global_result, v[0], v[1])
+	print(t)
+	#print(iter_count)
 	return
 
 PSO()
---
+++ Sem 1/Data Mining/EnsembleCancer.py
@@ -0,0 +1,84 @@
+import collections
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from pandas import DataFrame
+from sklearn.ensemble import VotingClassifier
+from sklearn.impute import SimpleImputer
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+from sklearn import metrics, model_selection
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.svm import SVC
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
+from sklearn.metrics import confusion_matrix
+
+data = pd.read_csv("/home/jerinpaul/Documents/ME/Sem 1/Data Mining/cancer.csv")
+data.drop(['Sample Code Number'], axis=1, inplace=True)
+
+#print(data.describe())
+#print(data.info())
+#print(data.head())
+data.replace('?', 0, inplace=True)
+
+values = data.values
+imputer = SimpleImputer()
+imputedData = imputer.fit_transform(values)
+
+scaler = MinMaxScaler(feature_range=(0, 1))
+normalizedData = scaler.fit_transform(imputedData)
+
+models = []
+models.append(('KNN', KNeighborsClassifier()))
+models.append(('LDA', LinearDiscriminantAnalysis()))
+models.append(('GNB', GaussianNB()))
+models.append(('LGR', LogisticRegression()))
+models.append(('DTC', DecisionTreeClassifier()))
+models.append(('SVC', SVC()))
+
+trained_models = []
+for model in models:
+    sample = DataFrame(normalizedData).sample(n=200)
+    sample = sample.to_numpy()
+    X = sample[:, 0:9]
+    Y = sample[:, 9]
+    trained_models.append(model[1].fit(X, Y))
+
+
+sample = DataFrame(normalizedData).sample(n=140)
+sample = sample.to_numpy()
+X = sample[:, 0:9]
+Y = sample[:, 9]
+
+mapping = []
+voted_pred = []
+for index, val in enumerate(Y):
+    x = X[index]
+    temp = collections.defaultdict(int)
+    for trained_model in trained_models:
+        predicted = trained_model.predict([x])
+        #print(predicted)
+        temp[predicted[0]] += 1
+    max_pred = max(temp.values())
+    result = None
+    for key in temp:
+        if temp[key] == max_pred:
+            result = key
+            break
+    voted_pred.append(result)
+
+print(round(metrics.accuracy_score(Y, voted_pred), 4))
+
+
+'''
+kfold = model_selection.KFold(n_splits=10, random_state=7, shuffle=True)
+ensemble = VotingClassifier(models)
+results = model_selection.cross_val_score(ensemble, X, Y, cv=kfold)
+print("Mean of all models' accuracy = " + str(results.mean()))
+'''
